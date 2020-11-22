9fe4c9f1b275796ca12550f5ca151de7ded31819
Андрій <Mula_jw0a@student.itstep.org>
2020-11-22 23:14:22 +0200

    fuh

---
+++ MissionLibTests/MissionLibTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MissionLib;
+
+namespace MissionLibTests
+{
+    [TestClass]
+    public class MissionLibTest
+    {
+        Missions missions;
+        [TestInitialize]
+        public void Init()
+        {
+            missions = new Missions();
+        }
+        
+
+        [TestMethod]
+        public void AddMission_Test()
+        {          
+            missions.AddMission(new Mission());
+            Assert.AreEqual(1, missions.AllMissions.Count);
+        }
+        [TestMethod]
+        public void RemoveMission_Test()
+        {
+            missions.AddMission(new Mission());
+            missions.RemoveMission(missions.AllMissions[0]);
+            Assert.AreEqual(0, missions.AllMissions.Count);
+        }
+
+        [TestMethod]
+        public void ChangeCheckState_Test()
+        {
+            missions.AddMission(new Mission());
+            missions.ChangeCheckState(0, true);
+            Assert.AreEqual(true, missions.AllMissions[0].State);
+        }
+
+        [TestMethod]
+        public void UpdateMission_Test()
+        {
+            missions.AddMission(new Mission());
+            missions.UpdateMission(0, new Mission("test", DateTime.Now, DateTime.Now, true));
+            Assert.AreEqual("test", missions.AllMissions[0].Title);
+        }
+    }
+}
---
+++ MissionLibTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MissionLibTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MissionLibTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98567ddc-518f-4e12-8a3e-79747e84034a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
